cc3722fc2dc8bc089c123dbb12b093dbbdddf128
Your New Username <johansierra222@gmail.com>
2024-01-26 15:00:58 -0500

    Finished about section added images

--- src/aboutBurger.js
+++ src/aboutBurger.js
@@ -79,3 +79,32 @@ setImage(projectLink, "../Images/projects.webp");
 setImage(skillsLink, "../Images/skills.webp");
 setImage(aboutLink, "../Images/about.png");
 setImage(contactLink, "../Images/contact.png");
+
+// ---------------------------------------------- CHATGPT CODE TO TOGGLE LANGUAGE -------------------------------------------------- //
+const englishLink = document.querySelector(".english");
+const spanishLink = document.querySelector(".spanish");
+let activeES = false;
+let activeEN = false;
+
+spanishLink.addEventListener("click", (e) => {
+    if (!spanishLink.classList.contains("active-language")) {
+        spanishLink.classList.add("active-language");
+        englishLink.classList.remove("active-language");
+        activeES = true;
+        activeEN = false;
+
+    }
+    console.log("Spanish:" + " " + activeES)
+    console.log("English:" + " " + activeEN)
+});
+
+englishLink.addEventListener("click", (e) => {
+    if (!englishLink.classList.contains("active-language")) {
+        englishLink.classList.add("active-language");
+        spanishLink.classList.remove("active-language");
+        activeES = false;
+        activeEN = true;
+    }
+    console.log("English:" + " " + activeEN)
+    console.log("Spanish:" + " " + activeES)
+});
